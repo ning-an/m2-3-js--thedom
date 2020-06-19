8b1c2184b6d0a530ef7736128718e70ee6be8726
Ning An <ning.an@outlook.com>
2020-06-19 15:46:49 -0400

    Update solution with challenges

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -25,39 +25,54 @@ while (nums.length < FROGS) {
 
 console.log(racers);
 
+//create final ranking board
+let main = document.querySelector('main');
+let finalBoard = document.createElement('div');
+finalBoard.classList.add('board');
+main.appendChild(finalBoard);
+
+let ranking = [];
+
 //Exercise 2.3
 racers.forEach(function(elem, index) {
     let racer = document.createElement('span');
-    racer.innerText = `${elem.name} ${elem.number}`;
+    racer.innerText = `${elem.name}\n${elem.number}`;
     racer.classList.add('frog');
     racer.classList.add(`frog-${elem.number}`)
-    racer.style.backgroundColor = elem.color;
+    // racer.style.backgroundColor = elem.color;
     document.querySelector(`#lane-${index + 1}`).appendChild(racer);
 
     elem.progress = 0;
     let racerProgress = document.createElement('span');
     racerProgress.innerText = `\t${elem.progress}`;
     document.querySelector(`#lane-${index + 1}`).appendChild(racerProgress);
+
+//Exercise 2.4 - ...
+    //frogs hop at random speed between 5% and 10% width
+    let distance = (Math.random() * 5 + 5) / 100;
+    let hop = setInterval(function() {
+        elem.progress += distance;
+        racerProgress.innerText = `${Math.round(elem.progress * 100)}%`;
+        if (elem.progress >= 1) {
+            elem.progress = 1;
+            racerProgress.innerText = '100%';
+            clearInterval(hop);
+            console.log(`Bravo! ${elem.name} has reached the end!`)
+            ranking.push(elem.name);
+            console.log(ranking);
+            if (ranking.length == 3) {
+                console.log(`Congratulations to ${ranking[0]}; ${ranking[1]} ranks 2nd; sorry for ${ranking[2]}`);
+                finalBoard.innerText = `No.1 ${ranking[0]}\nNo.2 ${ranking[1]}\nNo.3 ${ranking[2]}`;
+                finalBoard.style.opacity = 1;
+            }
+        }
+        document.querySelector(`.frog-${elem.number}`).style.left = `${elem.progress * 100}%`;
+    }, 500)
 })
+
 
 let cssLink = document.createElement('link');
 cssLink.rel = 'stylesheet';
 cssLink.href = '../assets/styles.css';
 document.querySelector('head').appendChild(cssLink);
 
-//Exercise 2.4
-function racingFrog(elem) {
-    //frogs hop at random speed between 5% and 10% width
-    let distance = (Math.random() * 5 + 5) / 100;
-    let hop = setInterval(function() {
-        elem.progress += distance;
-        if (elem.progress >= 1) {
-            elem.progress = 1;
-            clearInterval(hop);
-            console.log(`Bravo! ${elem.name} has reached the end!`)
-        }
-        document.querySelector(`.frog-${elem.number}`).style.left = `${elem.progress * 100}%`;
-    }, 500)
-}
-
-racers.forEach(racingFrog);
